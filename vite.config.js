4d5ff30ea5e65979d950649db18942c42225a616
ashish prajapat <ashishprajapat507@gmail.com>
2025-03-11 20:14:36 +0530

    update some new fetures

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensures it listens on all network interfaces
+    port: process.env.PORT || 3000, // Use the PORT environment variable (set by Render)
+  }
 })
